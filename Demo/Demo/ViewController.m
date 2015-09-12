32cede7c6a268d9b0efea28d3508e44cfa2eea2d
郑亚恒 <yahengzheng@163.com>
2015-09-12 22:49:24 +0800

    Xcode 6.4

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -15,13 +15,15 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor lightGrayColor];
     
     // 加载网络图片返回尺寸测试（图片网址可能失效，更换即可）
-    NSURL *url = [NSURL URLWithString:@"http://cdn.t04.pic.sogou.com/ac75323d6b6de243-503c0c74be6ae02f-fd60a6f0fbf944e3a2c80b2b3d36ae98.jpg"];
+    NSString *urlStr = @"http://cdn.t04.pic.sogou.com/ac75323d6b6de243-503c0c74be6ae02f-fd60a6f0fbf944e3a2c80b2b3d36ae98.jpg";
+    NSURL *url = [NSURL URLWithString:urlStr];
     
     // 获取图片尺寸
     [self getSize:url];
@@ -31,18 +33,18 @@
 }
 
 /// 获得图片大小
-- (void)getSize:(NSURL *)url {
-    
+- (void)getSize:(NSURL *)url
+{
     CGSize size = [YHGetNetPictureSize getImageSizeWithURL:url];
     NSLog(@"从服务器获得图片的尺寸--->%@",NSStringFromCGSize(size));
 }
 
 /// 显示图片验证
-- (void)displayImage:(NSURL*)url {
-    
+- (void)displayImage:(NSURL*)url
+{
     NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url cachePolicy:0 timeoutInterval:10];
     
-    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * data, NSURLResponse * response, NSError * error) {
 
         dispatch_async(dispatch_get_main_queue(), ^{
             
@@ -54,6 +56,5 @@
         
     }] resume];
 }
-
 
 @end
